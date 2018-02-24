1f20524cb7ecb86f52b0c5826372e7bc47768b2c
Nicolas Garcia-Autor 1 <nicoga97@gmail.com>
2018-02-23 19:58:04 -0500

    vista de registro al cliente autor 1

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,9 +5,14 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
+import edu.eci.pdsw.samples.entities.ItemRentado;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 /**
@@ -17,10 +22,90 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "AlquilerItems")
 @SessionScoped
 public class AlquilerItemsBean implements Serializable {
+    
+    private String nom;
+    private String em;
+    
+    
+    private String doc;
+    
+    private String tel;
+    private String dir;
+    
 
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
+    Cliente clienteSeleccionado;
 
+    public Cliente getClienteSeleccionado() {
+        return clienteSeleccionado;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEm() {
+        return em;
+    }
+
+    public void setEm(String em) {
+        this.em = em;
+    }
+
+    public String getDoc() {
+        return doc;
+    }
+
+    public void setDoc(String doc) {
+        this.doc = doc;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public ServiciosAlquiler getSp() {
+        return sp;
+    }
+
+    public void setSp(ServiciosAlquiler sp) {
+        this.sp = sp;
+    }
+
+    
+    
+
+    public void setClienteSeleccionado(Cliente clienteSeleccionado) {
+        this.clienteSeleccionado = clienteSeleccionado;
+    }
+
+    
     public AlquilerItemsBean() {
+    }
+    public void buttonListener() throws ExcepcionServiciosAlquiler{
+        sp.registrarCliente(new Cliente(nom,Integer.parseInt(doc),tel,dir,em));
+        nom="";
+        doc="";
+        tel="";
+        dir="";
+        em="";
+        
     }
 
 }
